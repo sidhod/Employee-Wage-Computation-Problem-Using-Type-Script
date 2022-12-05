a1a4d27f2631408157c15dc31705109d06ee86ca
unknown <sidhodhank2018@gmail.com>
2022-12-05 18:47:25 +0530

    [Sidhodhan]Added:Check Employee Attendance

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -5,3 +5,12 @@ console.log(""
     + " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
     + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
     + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
+var IS_Full_Time = 1;
+var empCheck = Math.floor(Math.random() * 2) + 0;
+console.log(empCheck);
+if (empCheck == IS_Full_Time) {
+    console.log("Employee is present");
+}
+else {
+    console.log("Employee is Absent");
+}
--- EmployeeWageComputation.ts
+++ EmployeeWageComputation.ts
@@ -5,3 +5,13 @@ console.log(""
     + " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
     + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
     + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
+let IS_Full_Time: number = 1;
+let empCheck: number = Math.floor(Math.random() * 2) + 0;
+console.log(empCheck);
+if (empCheck == IS_Full_Time) {
+    console.log("Employee is present");
+}
+else {
+    console.log("Employee is Absent");
+}
+
