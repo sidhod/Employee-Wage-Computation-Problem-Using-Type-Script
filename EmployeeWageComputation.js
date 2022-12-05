f4f57610add6e8b7cc29712884f59c71b21d3bb4
unknown <sidhodhank2018@gmail.com>
2022-12-05 18:57:54 +0530

    [Sidhodhan]Added:Calculating Employee Wage

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -6,11 +6,13 @@ console.log(""
     + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
     + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
 var IS_Full_Time = 1;
+var EMP_RATE_PER_HOUR = 20;
+var empHrs = 0;
+var empWage = 0;
 var empCheck = Math.floor(Math.random() * 2) + 0;
-console.log(empCheck);
-if (empCheck == IS_Full_Time) {
-    console.log("Employee is present");
-}
-else {
-    console.log("Employee is Absent");
-}
+if (empCheck == IS_Full_Time)
+    empHrs = 8;
+else
+    empHrs = 0;
+empWage = empHrs * EMP_RATE_PER_HOUR;
+console.log("Emp Wage: " + empWage);
--- EmployeeWageComputation.ts
+++ EmployeeWageComputation.ts
@@ -5,13 +5,15 @@ console.log(""
     + " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
     + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
     + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
-let IS_Full_Time: number = 1;
-let empCheck: number = Math.floor(Math.random() * 2) + 0;
-console.log(empCheck);
-if (empCheck == IS_Full_Time) {
-    console.log("Employee is present");
-}
-else {
-    console.log("Employee is Absent");
-}
+const IS_Full_Time: number = 1;
+const EMP_RATE_PER_HOUR: number = 20;
+let empHrs: number = 0;
+let empWage: number = 0;
 
+var empCheck = Math.floor(Math.random() * 2) + 0;
+if (empCheck == IS_Full_Time)
+    empHrs = 8;
+else
+    empHrs = 0;
+empWage = empHrs * EMP_RATE_PER_HOUR;
+console.log("Emp Wage: " + empWage);
