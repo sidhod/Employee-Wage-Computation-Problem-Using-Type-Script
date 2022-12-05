138620abb5e7dd8ef30102d7609ab89e2c29a6be
unknown <sidhodhank2018@gmail.com>
2022-12-05 19:09:52 +0530

    [Sidhodhan]Added:Add Part Time Employee Wage

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -6,11 +6,14 @@ console.log(""
     + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
     + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
 var IS_Full_Time = 1;
+var IS_Part_Time = 2;
 var EMP_RATE_PER_HOUR = 20;
 var empHrs = 0;
 var empWage = 0;
-var empCheck = Math.floor(Math.random() * 2) + 0;
-if (empCheck == IS_Full_Time)
+var empCheck = Math.floor(Math.random() * 3) + 0;
+if (empCheck === IS_Part_Time)
+    empHrs = 4;
+else if (empCheck === IS_Full_Time)
     empHrs = 8;
 else
     empHrs = 0;
--- EmployeeWageComputation.ts
+++ EmployeeWageComputation.ts
@@ -6,12 +6,15 @@ console.log(""
     + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
     + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
 const IS_Full_Time: number = 1;
+const IS_Part_Time: number = 2;
 const EMP_RATE_PER_HOUR: number = 20;
 let empHrs: number = 0;
 let empWage: number = 0;
 
-var empCheck = Math.floor(Math.random() * 2) + 0;
-if (empCheck == IS_Full_Time)
+var empCheck = Math.floor(Math.random() * 3) + 0;
+if (empCheck === IS_Part_Time)
+    empHrs = 4;
+else if (empCheck === IS_Full_Time)
     empHrs = 8;
 else
     empHrs = 0;
