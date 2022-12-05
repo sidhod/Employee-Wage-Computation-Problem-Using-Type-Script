9bd167c49b5df2d8017e00c363ea4e2f3ab0dc19
unknown <sidhodhank2018@gmail.com>
2022-12-05 19:38:17 +0530

    [Sidhodhan]Added:Calculate Total Employee Wage For Month

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -8,18 +8,24 @@ console.log(""
 var IS_Full_Time = 1;
 var IS_Part_Time = 2;
 var EMP_RATE_PER_HOUR = 20;
+var NO_OF_WORKING_DAYS = 20;
 var empHrs = 0;
 var empWage = 0;
-var empCheck = Math.floor(Math.random() * 3) + 0;
-switch (empCheck) {
-    case IS_Part_Time:
-        empHrs = 4;
-        break;
-    case IS_Full_Time:
-        empHrs = 8;
-        break;
-    default:
-        empHrs = 0;
+var totalEmployeeWage = 0;
+for (var day = 0; day <= NO_OF_WORKING_DAYS; day++) {
+    var empCheck = Math.floor(Math.random() * 3) + 0;
+    switch (empCheck) {
+        case IS_Part_Time:
+            empHrs = 4;
+            break;
+        case IS_Full_Time:
+            empHrs = 8;
+            break;
+        default:
+            empHrs = 0;
+    }
+    empWage = empHrs * EMP_RATE_PER_HOUR;
+    totalEmployeeWage = empWage + totalEmployeeWage;
+    console.log("Employee Wage is: " + empWage);
 }
-empWage = empHrs * EMP_RATE_PER_HOUR;
-console.log("Employee Wage is: " + empWage);
+console.log("Total Employee Wage of Month: " + totalEmployeeWage);
--- EmployeeWageComputation.ts
+++ EmployeeWageComputation.ts
@@ -8,19 +8,24 @@ console.log(""
 const IS_Full_Time: number = 1;
 const IS_Part_Time: number = 2;
 const EMP_RATE_PER_HOUR: number = 20;
+const NO_OF_WORKING_DAYS: number = 20;
 let empHrs: number = 0;
 let empWage: number = 0;
-
-var empCheck = Math.floor(Math.random() * 3) + 0;
-switch (empCheck) {
-    case IS_Part_Time:
-        empHrs = 4;
-        break;
-    case IS_Full_Time:
-        empHrs = 8;
-        break;
-    default:
-        empHrs = 0;
+let totalEmployeeWage: number = 0;
+for (let day = 0; day <= NO_OF_WORKING_DAYS; day++) {
+    var empCheck = Math.floor(Math.random() * 3) + 0;
+    switch (empCheck) {
+        case IS_Part_Time:
+            empHrs = 4;
+            break;
+        case IS_Full_Time:
+            empHrs = 8;
+            break;
+        default:
+            empHrs = 0;
+    }
+    empWage = empHrs * EMP_RATE_PER_HOUR;
+    totalEmployeeWage = empWage + totalEmployeeWage;
+    console.log("Employee Wage is: " + empWage);
 }
-empWage = empHrs * EMP_RATE_PER_HOUR;
-console.log("Employee Wage is: " + empWage);
+console.log("Total Employee Wage of Month: " + totalEmployeeWage);
