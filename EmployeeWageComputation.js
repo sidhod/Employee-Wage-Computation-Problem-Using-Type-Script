ec50c05ab85617c6c8b96bfad027a98af89bff38
unknown <sidhodhank2018@gmail.com>
2022-12-06 17:27:16 +0530

    [Sidhodhan]Refactor:Create Function To calculate Employee Wage

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,34 +1,43 @@
-console.log(""
-    + "    ##     ## ###### ##     ###### ###### ###   ### ######\r\n"
-    + " *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *\r\n"
-    + "*** ##  #  ## #####  ##     ##     ##  ## ##  #  ## #####  ***\r\n"
-    + " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
-    + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
-    + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
-var IS_Full_Time = 1;
-var IS_Part_Time = 2;
-var EMP_RATE_PER_HOUR = 20;
-var NO_OF_WORKING_DAYS = 20;
-var MAX_HRS_IN_MONTH = 100;
-var empHrs = 0;
-var empWage = 0;
-var totalEmployeeWage = 0;
-var totalEmpHrs = 0;
-var totalWorkingDays = 0;
-while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
-    totalWorkingDays++;
-    var empCheck = Math.floor(Math.random() * 3) + 0;
-    switch (empCheck) {
-        case IS_Part_Time:
-            empHrs = 4;
-            break;
-        case IS_Full_Time:
-            empHrs = 8;
-            break;
-        default:
-            empHrs = 0;
-    }
-    empWage = empHrs * EMP_RATE_PER_HOUR;
-    totalEmployeeWage = empWage + totalEmployeeWage;
+var fixedInfo;
+(function (fixedInfo) {
+    fixedInfo[fixedInfo["IS_Full_Time"] = 1] = "IS_Full_Time";
+    fixedInfo[fixedInfo["IS_Part_Time"] = 2] = "IS_Part_Time";
+    fixedInfo[fixedInfo["EMP_RATE_PER_HOUR"] = 20] = "EMP_RATE_PER_HOUR";
+    fixedInfo[fixedInfo["NO_OF_WORKING_DAYS"] = 20] = "NO_OF_WORKING_DAYS";
+    fixedInfo[fixedInfo["MAX_HRS_IN_MONTH"] = 100] = "MAX_HRS_IN_MONTH";
+})(fixedInfo || (fixedInfo = {}));
+function welcomMessage() {
+    console.log(""
+        + "    ##     ## ###### ##     ###### ###### ###   ### ######\r\n"
+        + " *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *\r\n"
+        + "*** ##  #  ## #####  ##     ##     ##  ## ##  #  ## #####  ***\r\n"
+        + " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
+        + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
+        + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
 }
-console.log("Total Employee Wage of Month: " + totalEmployeeWage);
+function calculateEmployeeWage() {
+    var empHrs = 0, empWage = 0, totalEmployeeWage = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+    while (totalEmpHrs < fixedInfo.MAX_HRS_IN_MONTH && totalWorkingDays < fixedInfo.NO_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        var empCheck = Math.floor(Math.random() * 3) + 0;
+        switch (empCheck) {
+            case fixedInfo.IS_Part_Time:
+                empHrs = 4;
+                break;
+            case fixedInfo.IS_Full_Time:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+        empWage = empHrs * fixedInfo.EMP_RATE_PER_HOUR;
+        totalEmployeeWage = empWage + totalEmployeeWage;
+    }
+    return totalEmployeeWage;
+}
+function showresult(result) {
+    console.log("***********|Total Employee Wage of Month: " + result + "|**********");
+}
+welcomMessage(); //Call welome message
+var result = calculateEmployeeWage(); //call calculateEmployeeWage Function and take in variable
+showresult(result);
--- EmployeeWageComputation.ts
+++ EmployeeWageComputation.ts
@@ -1,34 +1,42 @@
-console.log(""
-    + "    ##     ## ###### ##     ###### ###### ###   ### ######\r\n"
-    + " *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *\r\n"
-    + "*** ##  #  ## #####  ##     ##     ##  ## ##  #  ## #####  ***\r\n"
-    + " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
-    + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
-    + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
-const IS_Full_Time: number = 1;
-const IS_Part_Time: number = 2;
-const EMP_RATE_PER_HOUR: number = 20;
-const NO_OF_WORKING_DAYS: number = 20;
-const MAX_HRS_IN_MONTH: number = 100;
-let empHrs: number = 0;
-let empWage: number = 0;
-let totalEmployeeWage: number = 0;
-let totalEmpHrs = 0;
-let totalWorkingDays = 0;
-while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
-    totalWorkingDays++;
-    var empCheck = Math.floor(Math.random() * 3) + 0;
-    switch (empCheck) {
-        case IS_Part_Time:
-            empHrs = 4;
-            break;
-        case IS_Full_Time:
-            empHrs = 8;
-            break;
-        default:
-            empHrs = 0;
-    }
-    empWage = empHrs * EMP_RATE_PER_HOUR;
-    totalEmployeeWage = empWage + totalEmployeeWage;
+enum fixedInfo {
+    IS_Full_Time = 1,
+    IS_Part_Time = 2,
+    EMP_RATE_PER_HOUR = 20,
+    NO_OF_WORKING_DAYS = 20,
+    MAX_HRS_IN_MONTH = 100,
 }
-console.log("Total Employee Wage of Month: " + totalEmployeeWage);
+function welcomMessage(): void {
+    console.log(""
+        + "    ##     ## ###### ##     ###### ###### ###   ### ######\r\n"
+        + " *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *\r\n"
+        + "*** ##  #  ## #####  ##     ##     ##  ## ##  #  ## #####  ***\r\n"
+        + " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
+        + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
+        + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
+}
+function calculateEmployeeWage(): number {
+    let empHrs: number = 0, empWage: number = 0, totalEmployeeWage: number = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+    while (totalEmpHrs < fixedInfo.MAX_HRS_IN_MONTH && totalWorkingDays < fixedInfo.NO_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        var empCheck = Math.floor(Math.random() * 3) + 0;
+        switch (empCheck) {
+            case fixedInfo.IS_Part_Time:
+                empHrs = 4;
+                break;
+            case fixedInfo.IS_Full_Time:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+        empWage = empHrs * fixedInfo.EMP_RATE_PER_HOUR;
+        totalEmployeeWage = empWage + totalEmployeeWage;
+    }
+    return totalEmployeeWage;
+}
+function showresult(result: number): void {
+    console.log("***********|Total Employee Wage of Month: " + result + "|**********");
+}
+welcomMessage();//Call welome message
+let result: number = calculateEmployeeWage();//call calculateEmployeeWage Function and take in variable
+showresult(result);
